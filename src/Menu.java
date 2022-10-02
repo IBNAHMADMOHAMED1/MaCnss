f4043d981d081c4797765e741242771225b3b682
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-02 20:22:41 +0100

    Create Simple Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Menu {
+
+import java.util.Scanner;
+
+public class Menu {
+    public static  int showHome() {
+        System.out.println("1- Login");
+        System.out.println("2- Register");
+        System.out.println("3- Exit");
+        System.out.print("Enter your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        return choice;
+    }
+
 }
