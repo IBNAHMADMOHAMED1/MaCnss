062b9e569db3106fcfcf8bc2ad3de2e1c59bdb49
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-02 19:45:21 +0100

    Create a Class to connect to database

---
+++ src/databaes/DbConncetion.java
@@ -0,0 +1,21 @@
+package databaes;
+import java.sql.*;
+
+public class DbConncetion {
+
+    private  static final String databaseName = "MaCnss";
+    private static final String user = "root";
+    private static final String password = "123456";
+
+    // this method to connect to database and return connection
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/"+databaseName,user,password);
+    }
+
+    // this method to return statement
+    public static Statement getStatement() throws SQLException {
+        return getConnection().createStatement();
+    }
+
+
+}
