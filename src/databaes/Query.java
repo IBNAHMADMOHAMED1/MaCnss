92e6ec7abd6a545465315563a352a57cbdc99722
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-02 19:46:21 +0100

    Create a class Query to execute queries

---
+++ src/databaes/Query.java
@@ -0,0 +1,24 @@
+package databaes;
+import java.sql.*;
+
+public class Query extends DbConncetion{
+    // this class to make query to database and return result (insert,update,delete,select)
+    // this method to insert data to database
+    public static void insert(String query) throws SQLException {
+        getStatement().executeUpdate(query);
+    }
+    // this method to update data to database
+    public static void update(String query) throws SQLException {
+        getStatement().executeUpdate(query);
+    }
+    // this method to delete data to database
+    public static void delete(String query) throws SQLException {
+        getStatement().executeUpdate(query);
+    }
+    // this method to select data from database
+    public static ResultSet select(String query) throws SQLException {
+        return getStatement().executeQuery(query);
+    }
+
+
+}
