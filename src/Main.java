b162b9da104afcbfc2dda0f02079bc614e676c43
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-03 15:48:38 +0100

    End For email sending

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
        int choice = Menu.showHome();
          switch (choice) {
               case 1:
                 LoginAgentCnss.login();
-                if (LoginAgentCnss.isLogin()) {
+                if (LoginAgentCnss.isLogin) {
                     System.out.println("Welcome to the system");
+
                 }
                 break;
               case 2:
                // Register.register();
+
                 break;
               case 3:
                 System.exit(0);
