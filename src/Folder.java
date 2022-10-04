56909f6aba56d0dc4dd58039a01a155ffa7342b5
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-04 10:39:55 +0100

    Create Class Folder

---
+++ src/Folder.java
@@ -0,0 +1,20 @@
+import document.*;
+
+import java.util.*;
+
+public class Folder {
+    private String folderCode;
+    private String typeOfArrangement;
+    private String statusFolder;
+    private String patientMatricNo;
+    private Double totalAmount = 0.0;
+    private Double totalAmountReimbursed = 0.0;
+    private List<Medicament> medicaments = new ArrayList<Medicament>();
+    private Analysis analysis;
+    private Radio radio;
+
+
+
+
+
+}
---
+++ src/document/Radio.java
@@ -0,0 +1,2 @@
+package document;public class Radio {
+}
