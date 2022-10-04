716406dbb0c22bc65d44ef751135ca76ac02edd7
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-04 11:56:47 +0100

    Creat a patient class

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Patient {
+import databaes.Query;
+
+import java.sql.ResultSet;
+
+public class Patient {
+
+    public static Boolean isPatientExist(String patientMatricNo) {
+        try {
+            ResultSet resultSet = Query.select("select * from Patient where PatientMatricNo = '"+patientMatricNo+"'");
+            if (resultSet.next()) {
+                return true;
+            }
+            return false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    // get id of patient by matricNo
+    public static int getPatientId(String patientMatricNo) {
+        try {
+            ResultSet resultSet = Query.select("select * from Patient where PatientMatricNo = '"+patientMatricNo+"'");
+            if (resultSet.next()) {
+                return resultSet.getInt("PatientId");
+            }
+            return 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
