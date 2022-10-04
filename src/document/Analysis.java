4d2684681006d0c89b2c02b21a6c37a2174a5a50
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-04 10:40:45 +0100

    Create Class Analysis

--- src/document/Analysis.java
+++ src/document/Analysis.java
@@ -1,2 +1,9 @@
-package document;public class Analysis {
+package document;
+
+public class Analysis implements Reimbursed {
+    private Double baseReimbursementPrice = 0.2;
+
+    public Double getTotalAmountReimbursed() {
+        return baseReimbursementPrice;
+    }
 }
