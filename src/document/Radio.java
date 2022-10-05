f5f53283a7c564e10fd9b3faf2dfbcc0376dac0f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-05 15:19:27 +0100

    Create Methode of rate

--- src/document/Radio.java
+++ src/document/Radio.java
@@ -1,5 +1,16 @@
 package document;
 
 public class Radio {
-    private Double baseReimbursementPrice = 0.4;
+    private static Double baseReimbursementPrice = 0.4;
+    private static final Double BASE_REIMBURSEMENT_PRICE = 3000.0;
+
+    // getAmountReimbursed
+    public static Double getAmountReimbursed(Double amount) {
+        if (amount > BASE_REIMBURSEMENT_PRICE) {
+            return BASE_REIMBURSEMENT_PRICE * baseReimbursementPrice;
+        } else {
+            return amount * baseReimbursementPrice;
+        }
+    }
+
 }
