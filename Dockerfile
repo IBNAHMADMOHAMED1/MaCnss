52920093d87c38e10c3ea034bd987c83d847f2d5
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-05 11:29:56 +0100

    Create Medicamet logique

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ubuntu:18.04
+
+MAINTAINER ibnahmad Mohamed <ibnahmadmohamed8@gmail.com>
+
+RUN apt-get update && apt-get install -y  default-jdk
--- src/document/Medicament.java
+++ src/document/Medicament.java
@@ -1,5 +1,54 @@
 package document;
 
+import databaes.Query;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Medicament {
+    private Double medicament_rate;
+    private String medicament_code;
+
+
+    public  void addMedicament(String codeFolder,String medicamentCode){
+        // wen to get Rate of medicament
+        Double medicamentRate = Medicament.getMedicamentByField(medicamentCode,"medicament_rate");
+        Boolean isInserted = saveMedicament(codeFolder,medicamentRate,medicamentCode);
+
+
+    }
+
+    public Boolean  saveMedicament(String codeFolder , Double medicamentRate , String medicamentCode){
+        String query = "INSERT INTO medicamentList (folder_code,medicament_rate,medicament_code) VALUES ('"+codeFolder+"','"+medicamentRate+"','"+medicamentCode+"')";
+        try {
+            Query.insert(query);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    // getMedicamentRate
+    public static Double getMedicamentByField(String medicamentCode,String field){
+
+        try {
+            ResultSet resultSet = Query.select("SELECT "+field+" FROM medicament_list WHERE medicament_code = '"+medicamentCode+"'");
+            if (resultSet.next()) {
+                return resultSet.getDouble(field);
+            } else {
+                return 0.0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0.0;
+        }
+    }
+
+
+
 
 }
