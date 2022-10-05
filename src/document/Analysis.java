5ee31458f2281efe6d996d41ca44a268c5458d09
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-05 15:19:17 +0100

    Create Methode of rate

--- src/document/Analysis.java
+++ src/document/Analysis.java
@@ -1,9 +1,18 @@
 package document;
 
 public class Analysis implements Reimbursed {
-    private Double baseReimbursementPrice = 0.2;
+    private static Double baseReimbursementPrice = 0.3;
+    private static final Double BASE_REIMBURSEMENT_PRICE = 1000.0;
 
     public Double getTotalAmountReimbursed() {
         return baseReimbursementPrice;
+    }
+
+    public static Double getAmountReimbursed(Double amount) {
+        if (amount > BASE_REIMBURSEMENT_PRICE) {
+            return 1000 * baseReimbursementPrice;
+        } else {
+            return amount * baseReimbursementPrice;
+        }
     }
 }
